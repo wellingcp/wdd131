523bd72e22a9b752caa819ae882d641d6a2e1b3f
wellingcp <wellingcp@gmail.com>
2026-09-21 19:40:38 -0600

    js variables and constants

---
+++ ponder/week1.js
@@ -0,0 +1,23 @@
+const PI = 3.14;
+let radius = 3;
+
+console.log(PI);
+console.log(radius);
+
+const one = 1;
+const two = "2";
+
+console.log(one);
+console.log(two);
+
+let course = "CSE131";
+
+if (true) {
+    let student = "John";
+
+    console.log(course);
+    console.log(student);
+}
+
+console.log(course);
+console.log(student);
